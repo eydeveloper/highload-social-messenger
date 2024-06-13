df5e75a3acaee624d4da39d81e92a4eddf00d28e
Egor Yaschenko <egor.yaschenko@altenar.com>
2024-06-13 23:11:37 +0300

    Add tracing with X-Request-ID

--- internal/handler/messenger.go
+++ internal/handler/messenger.go
@@ -3,11 +3,16 @@ package handler
 import (
 	"github.com/eydeveloper/highload-social-messenger/internal/entity"
 	"github.com/gin-gonic/gin"
+	"github.com/sirupsen/logrus"
 	"net/http"
 )
 
 func (h *Handler) sendMessage(c *gin.Context) {
 	senderId := c.MustGet("userId").(string)
+	requestId := c.MustGet("X-Request-ID").(string)
+
+	logrus.Info("Handling send message request with ID: " + requestId)
+
 	var message entity.Message
 
 	if err := c.BindJSON(&message); err != nil {
@@ -30,6 +35,9 @@ func (h *Handler) sendMessage(c *gin.Context) {
 func (h *Handler) getMessages(c *gin.Context) {
 	senderId := c.MustGet("userId").(string)
 	receiverId := c.Param("id")
+	requestId := c.MustGet("X-Request-ID").(string)
+
+	logrus.Info("Handling get messages request with ID: " + requestId)
 
 	messages, err := h.services.GetMessages(senderId, receiverId)
 
